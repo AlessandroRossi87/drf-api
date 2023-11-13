c93ec67f8e88ddb5615b291a9737db65dac65c2b
Alessandro Rossi <rossisverige@yahoo.se>
2023-11-13 10:36:52 +0000

    Setting up environ

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,7 +6,7 @@ class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ReadOnlyField(sourche='owner.profile.image.url')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -24,7 +24,7 @@ class PostSerializer(serializers.ModelSerializer):
         return value
 
     def get_is_owner(self, obj):
-        request. self.context['request']
+        request = self.context['request']
         return request.user == obj.owner
 
     class Meta:
